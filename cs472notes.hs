db3d8386141154fcd2bb18adb5f1c2a07bf2d130
Jong <parkj6@oregonstate.edu>
2019-04-14 20:53:58 -0700

    HW1 added

---
+++ cs472notes.hs
@@ -0,0 +1,85 @@
+CS 472 - Computer Architecture
+MW 8:30am-10am - Gilfillan Auditorium
+
+----------------------------
+--  2019-04-01 --- Wk1 D1 --
+----------------------------
+
+--| Logistics:
+https://oregonstate.instructure.com/courses/1716181/assignments/syllabus
+
+--Prof info:
+use [CA-S19] tag for emails
+chenliz@oregonstate.edu
+Office hours: W 1-2PM KEC 3113
+
+--TA:
+    Paris Kalathas
+    Tingwei Zhang
+    Jia Guo
+    Aashish Adhikari
+All TA office hours location: KEC Atrium
+Mon:
+    08:00 - 09:50 CLASS
+    1.5h TA
+Tue:
+    1.5h TA
+    1.5h TA
+Wed:
+    08:00 - 09:50 CLASS
+    13:00 - 14:00 Prof. Chen (KEC 3113)
+    1.5h TA
+Thur:
+    1.5h TA
+Fri:
+    14:30 - 17:30 Jia Guo
+
+-- Tests
+Midterm 1: May 1 (Wed)  -- easier
+    4/1 - 4/29 will be covered
+Midterm 2: May 29 (Wed) -- difficult.
+    5/6 - 5/22 will be covered
+Final: none.
+
+-- Textbook:
+David Patterson and John Hennessy, Computer Organization and Design, 5th Edition, Morgan Kaufmann, 2013, ISBN 9780124077263
+
+-- 3 HW, due beginning of class.
+Light load of lab.
+20% penalty per day.
+Everything is paper based. 
+
+--Grade Breakdown:
+HW: 20%
+Midterms: 30%, 30%
+Lab: 10%
+Class Participation: 10%
+    (ask q, attendance)
+Grades will be curved up, not down:
+>= 93: A
+>= 83: B
+    otherwise:
+    30-40%:  A, A-
+    50%:     B+, B, B-
+    10-20%:  < B-
+    Missing lab or midterm: F
+Extra Credit offered.
+
+
+----------------------------
+--  2019-04-03 --- Wk1 D2 --
+----------------------------
+
+
+Moore's Law is Dead
+
+https://computing.ilnl.gov/tutorials/bgq
+
+IBM Blue Gene/Q Supercomputer takes about 20MW
+(Corvallis takes about 5.5 - 6MW)
+
+The voltage is low and can't go lower than the minimum required voltage.
+
+
+Instruction Set has nothing to do with processor
+
